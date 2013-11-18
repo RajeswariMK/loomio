812b10aabb33226c7c9b26dbc527d6083b3058a2
john irving <whimful@gmail.com>
2013-11-19 10:02:17 +1300

    cuke fix

--- features/step_definitions/view_discussions_as_new_user.rb
+++ features/step_definitions/view_discussions_as_new_user.rb
@@ -1,5 +1,5 @@
 Then /^I should see the decision$/ do
-  page.should have_content(@motion.discussion.title)
+  page.should have_content(@motion.discussion.title[0..25])
 end
 
 Then /^I should see an empty discussion list$/ do
